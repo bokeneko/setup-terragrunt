2d0fa35ebc519857567be2bb2edf725c6e670af9
bokeneko <beerbierbear+github@uzzyneko.com>
2021-07-06 12:24:22 +0900

    correct tool-cache usage

--- dist/index.js
+++ dist/index.js
@@ -34,27 +34,7 @@ function mapOS(os) {
     return mappings[os] || os;
 }
 
-async function downloadCLI(url) {
-    core.debug(`Downloading Terragrunt CLI from ${url}`);
-    const fileName = path.basename(url);
-    const exeSuffix = fileName.endsWith(".exe") ? '.exe' : '';
-    const pathToCLI = await tc.downloadTool(url);
-    if (!pathToCLI) {
-        throw new Error(`Unable to download Terragrunt from ${url}`);
-    }
-    try {
-        source = [pathToCLI, fileName].join(path.sep);
-        target = [pathToCLI, `terragrunt${exeSuffix}`].join(path.sep);
-        core.debug(`Moving ${source} to ${target}.`);
-        await io.mv(source, target);
-    } catch (e) {
-        core.error(`Unable to move ${source} to ${target}.`);
-        throw e;
-    }
-    return pathToCLI;
-}
-
-async function getTerragruntUrl(version, platform, arch) {
+async function downloadCLI(version, platform, arch) {
     let tg_version = version;
     if (tg_version == 'latest') {
         core.debug("Checking the latest version of Terragrunt");
@@ -63,9 +43,15 @@ async function getTerragruntUrl(version, platform, arch) {
         core.debug(`using ${tg_version}`);
     }
     const exeSuffix = platform.startsWith('win') ? '.exe' : '';
-
     const url = `https://github.com/gruntwork-io/terragrunt/releases/download/${tg_version}/terragrunt_${platform}_${arch}${exeSuffix}`;
-    return url;
+
+    core.debug(`Downloading Terragrunt CLI from ${url}`);
+    const pathToCLI = await tc.downloadTool(url);
+    if (!pathToCLI) {
+        throw new Error(`Unable to download Terragrunt from ${url}`);
+    }
+    const cachedPath = await tc.cacheFile(pathToCLI, `terragrunt${exeSuffix}`, 'terragrunt', 'tg_version');
+    return cachedPath;
 }
 
 async function run() {
@@ -79,17 +65,14 @@ async function run() {
         const platform = mapOS(osPlatform);
         const arch = mapArch(osArch);
 
-        // create download url
-        core.debug(`Getting url for Terragrunt version ${version}: ${platform} ${arch}`);
-        const url = await getTerragruntUrl(version, platform, arch);
-
         // Download requested version
-        const pathToCLI = await downloadCLI(url);
+        core.debug(`Getting url for Terragrunt version ${version}: ${platform} ${arch}`);
+        const pathToCLI = await downloadCLI(version, platform, arch);
 
         // Add to path
         core.addPath(pathToCLI);
 
-        return release;
+        return pathToCLI;
     } catch (error) {
         core.error(error);
         throw error;
